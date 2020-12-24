4c455db3b3f63f2394e24e3dd8562ff52cfefa81
Ray Luna <leon.luna.ray@gmail.com>
2020-12-23 22:02:28 -0800

    Debugged the UV api functionality and color change.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#search-button").on("click", function(){
-        const userCity = "portland" //$("#city-search").val(); temp change
-        searchedCity(userCity)
+        const userCity = "portland" //$("#city-search").val(); temp change for testing purposes
+        searchedCity(userCity);
 });
 
 
@@ -10,11 +10,11 @@ function searchedCity(userCity) {
 const apiKey = "40d735c04b7e328dff495db17c0a745b";
 const cityQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&units=" + dataUnits + "&appid=" + apiKey;
 
-// Current conditions
+// Render weather data for the searched city.
 $.ajax( {
     url: cityQueryURL
-}).then(function(data) {
-    //weather data card
+}).then(function getWeather(data) { // Will there be an issue with naming this function?
+    //City weather data card.
     const cityName = $("<h1>").addClass("city-name").text(data.name);
     const cityOneDayDiv = $("<div>").addClass("one-day-card");
     const cityWindSpeed = $("<p>").addClass("wind-data").text(data.wind.speed.toFixed() + " mph");
@@ -22,39 +22,44 @@ $.ajax( {
     const cityTemp = $("<p>").addClass("city-temprature").text(data.main.temp.toFixed()  + "°F");
     const cardBody = $("<div>").addClass("card-body");
     const weatherImage = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-    cityName.append(weatherImage)
-    cardBody.append(cityName, cityTemp, cityHumidity, cityWindSpeed)
-    cityOneDayDiv.append(cardBody)
-    $(".city-data").append(cityOneDayDiv)
+    cityName.append(weatherImage);
+    cardBody.append(cityName, cityTemp, cityHumidity, cityWindSpeed);
+    cityOneDayDiv.append(cardBody);
+    $(".city-data").append(cityOneDayDiv);
 
-    // get uv function
-    const cityLat = data.coord.lat;
-    const cityLon = data.coord.lon;
-    getUV(data.coord.lat, data.coord.lon);
+    // UV API call
     function getUV(lat, lon){
         $.ajax({
         url: uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
     }).then(function(data) {
+        // Append UV data to card body
         const uvData = data.value;
         const cityUV = $("<p>").addClass("city-uv").text(uvData);
         $(cardBody).append(cityUV);
+
         // Change UV index color 
-        // if (uvData < 4){
-        //     console.log("less than 4")
-        //     $(".city-uv").attr("id" , "uv-green");
-        // } else if (uvData >= 4 && uvData <= 8){
-        //     $(".city-uv").attr("id", "uv-yellow");
-        // } else if (uvData > 8){
-        //     $(".city-uv").attr("id", "uv-red");
-        // }
+        if (uvData < 4) {
+            $(".city-uv").attr("id", "uv-green");
+        } else if (uvData >= 4 && uvData <= 8) {
+            $(".city-uv").attr("id", "uv-yellow");
+        } else if (uvData > 8) {
+            $(".city-uv").attr("id", "uv-red");
+        }
     });
-    };
+    }; // getUV function
+
+    getUV(data.coord.lat, data.coord.lon); // Invoke the UV function with coordinate data for location.
+
+    // TODO:
+    // get forcast function loop.
+    // history or local storage at the very least.
+    // stying issues if time available.
 
 
 
-    // get forcast function loop
-    // history?
 
+
+    
 }); //ajax
 };  //searched city
 }); // wrapper
@@ -65,24 +70,6 @@ $.ajax( {
 
 
 
-//     // UV API
-//     $.ajax({
-//         url: uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + apiKey
-//     }).then(function(data) {
-//         const cityUV = data.value;
-//         $(".city-uv").append(cityUV);
-
-//         // Change UV index color 
-//         if (cityUV < 4){
-//             $(".city-uv").attr("class" , "uv-green");
-//         } else if (cityUV >= 4 && cityUV <= 8){
-//             $(".city-uv").attr("class", "uv-yellow");
-//         } else if (cityUV > 8){
-//             $(".city-uv").attr("class", "uv-red");
-//         }
-//     });
-// });
-// }; // Searched city
 
 // // Forcast API
 // const forcastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&units=" + dataUnits + "&appid=" + apiKey;
@@ -104,5 +91,3 @@ $.ajax( {
         // } // need to figure out how to get five days to render
 
 // });
-
-// }); //Wrapper
