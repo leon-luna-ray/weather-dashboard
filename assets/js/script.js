3948208cc35f41bbb1027d72025e2eb8f3076d15
Ray Luna <leon.luna.ray@gmail.com>
2020-12-26 17:10:44 -0800

    Added functionality to load Portland if no previous searches on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
+    const apiKey = "40d735c04b7e328dff495db17c0a745b";
+    const dataUnits = "imperial"; // Possibility to add metric functionality later.
+
+    // If there are no previous searches, load weather for Portland otherwise load the last searched city. Possibility to later add functionality to load City based on the user's location.
+    if (window.localStorage.getItem("stored-cites") === null) {
+        cityWeather("Portland", apiKey, dataUnits);
+        cityForecast("Portland", apiKey, dataUnits);
+    } else {
+
+        cityWeather(userCity, apiKey, dataUnits);
+        cityForecast(userCity, apiKey, dataUnits);
+    };
+
     // Event listener for searched city.
     $("#search-button").on("click", function citySearch(){
         const userCity = $("#city-search").val();
-        const apiKey = "40d735c04b7e328dff495db17c0a745b";
-        const dataUnits = "imperial"; // Possibility to add metric functionality later.
 
         // Invoke current weather and forecast functions.
         saveCity(userCity);
@@ -17,7 +28,6 @@ $(document).ready(function(){
         citiesArr.push(city)
 
         // If local storage is empty create new key, else add the current search.
-
         if(window.localStorage.getItem("stored-cites") === null) {
             localStorage.setItem("stored-cites", citiesArr);
         } else {
