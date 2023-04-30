7dcc40651a37a041efc4f217dd6b5f7b7646b69d
Ray Luna <leon.luna.ray@gmail.com>
2023-04-29 20:38:30 -0700

    update query string to api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <script setup>
-import { computed, onBeforeMount, ref } from 'vue';
+import { computed, onBeforeMount, reactive, ref } from 'vue';
 import axios from 'axios'
 
-const key = import.meta.env.VITE_APP_API_KEY
+const apiKey = import.meta.env.VITE_APP_API_KEY
+const baseApiUrl = `http://api.openweathermap.org`
 
 // State
-const weatherData = ref(null);
+const state = reactive({
+  isLoading: false,
+  error: null,
+  data: null,
+});
+const city = ref('portland');
 
 // Computed
-const cityName = computed(()=>{
-  return weatherData.value.city.name;
+const cityName = computed(() => {
+  return state.data.city.name;
 })
 
+// Methods
+const forecastQueryStr = (query) => {
+  return `${baseApiUrl}/data/2.5/forecast?q=${query}&appid=${apiKey}`
+}
+const coordinatesQueryStr = (query) => {
+  // {city name},{state code},{country code}
+  return `${baseApiUrl}/geo/1.0/direct?q=${query}&limit=1&appid=${apiKey}`
+}
+const fetchForecastData = async (query) => {
+  state.isLoading = true;
+
+  try {
+    const response = await axios.get(forecastQueryStr(query));
+    state.data = response.data;
+  } catch (error) {
+    state.error = error.message;
+  } finally {
+    state.isLoading = false;
+  }
+};
+
 onBeforeMount(async () => {
-  const query = `https://api.openweathermap.org/data/2.5/forecast?q=portland&appid=${key}`
-  const res = await axios.get(query)
-  weatherData.value = res.data
+  fetchForecastData(city.value);
 })
 </script>
 
 <template>
-  <div v-if="weatherData">
+  <div v-if="state.data">
     <h1>Weather Dashboard</h1>
     <h2>{{ cityName }}</h2>
   </div>
