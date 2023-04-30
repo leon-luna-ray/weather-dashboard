74fe616950128f969f22b22241b84950657253f1
Ray Luna <leon.luna.ray@gmail.com>
2023-04-29 21:23:14 -0700

    update base api call

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { computed, onBeforeMount, reactive, ref } from 'vue';
 import axios from 'axios'
 
 const apiKey = import.meta.env.VITE_APP_API_KEY
-const baseApiUrl = `http://api.openweathermap.org`
+const baseApiUrl = `https://api.openweathermap.org`
 
 // State
 const state = reactive({
@@ -12,6 +12,10 @@ const state = reactive({
   data: null,
 });
 const city = ref('portland');
+const coorinates = ref({
+  lat: 0,
+  lon: 0,
+});
 
 // Computed
 const cityName = computed(() => {
@@ -22,16 +26,25 @@ const cityName = computed(() => {
 const forecastQueryStr = (query) => {
   return `${baseApiUrl}/data/2.5/forecast?q=${query}&appid=${apiKey}`
 }
+const currentDataStr = (lat, lon) => {
+  return `${baseApiUrl}/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+}
 const coordinatesQueryStr = (query) => {
   // {city name},{state code},{country code}
   return `${baseApiUrl}/geo/1.0/direct?q=${query}&limit=1&appid=${apiKey}`
 }
 const fetchForecastData = async (query) => {
   state.isLoading = true;
-
   try {
-    const response = await axios.get(forecastQueryStr(query));
-    state.data = response.data;
+    const response = await axios.get(coordinatesQueryStr(query));
+
+    coorinates.value.lat = response.data[0].lat;
+    coorinates.value.lon = response.data[0].lon;
+
+
+    // const forecast = await axios.get(oneCallQueryStr(coorinates.value.lat, coorinates.value.lon))
+    // console.log(forecast)
+    console.log(oneCallQueryStr(coorinates.value.lat, coorinates.value.lon))
   } catch (error) {
     state.error = error.message;
   } finally {
@@ -39,7 +52,8 @@ const fetchForecastData = async (query) => {
   }
 };
 
-onBeforeMount(async () => {
+// Lifecycle
+onBeforeMount(() => {
   fetchForecastData(city.value);
 })
 </script>
