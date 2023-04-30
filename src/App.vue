321ce4bd8ff8613b1071b72310070914f8989770
Ray Luna <leon.luna.ray@gmail.com>
2023-04-29 18:22:01 -0700

    API call setup and base styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, onBeforeMount, ref } from 'vue';
+import axios from 'axios'
+
+const key = import.meta.env.VITE_APP_API_KEY
+
+// State
+const weatherData = ref(null);
+
+// Computed
+const cityName = computed(()=>{
+  return weatherData.value.city.name;
+})
+
+onBeforeMount(async () => {
+  const query = `https://api.openweathermap.org/data/2.5/forecast?q=portland&appid=${key}`
+  const res = await axios.get(query)
+  weatherData.value = res.data
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div v-if="weatherData">
+    <h1>Weather Dashboard</h1>
+    <h2>{{ cityName }}</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
