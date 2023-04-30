8f34ebc0d3ab37e2d01f29947a9bcb930b846ef2
Ray Luna <leon.luna.ray@gmail.com>
2023-04-29 17:56:17 -0700

    install tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
+  <p class="text-3xl font-bold underline">
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
