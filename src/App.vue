bddd8d92562fc399e49af9c3764fb131382b6174
Ray Luna <leon.luna.ray@gmail.com>
2023-05-02 00:02:24 -0700

    add computed props for current data

--- src/App.vue
+++ src/App.vue
@@ -2,51 +2,93 @@
 import { computed, onBeforeMount, reactive, ref, watch } from 'vue';
 import axios from 'axios'
 
+import CurrentPanel from './components/CurrentPanel.vue';
+
 const apiKey = import.meta.env.VITE_APP_API_KEY
 const baseApiUrl = `https://api.openweathermap.org`
 
 // State
+const city = ref('portland');
+const useMetricUnits = ref(false);
+
 const state = reactive({
   error: null,
   current: null,
   forecast: null,
   uv: null,
 });
-const city = ref('portland');
 
 // Computed
 const cityName = computed(() => {
-  if (!state.current) return null;
-  return state.current.name;
+  const { name } = state.current || '';
+  return name;
 })
 const coordinates = computed(() => {
-  if (!state.current) return null;
-  return state.current.coord;
+  const { coord } = state.current || {};
+  return coord;
+})
+const currentDescription = computed(() => {
+  return state.current?.weather[0].description;
+})
+const currentWeather = computed(() => {
+  return state.current.weather[0].main;
+})
+const currentIconUrl = computed(() => {
+  const iconCode = state.current.weather[0].icon || '#'
+  return `http://openweathermap.org/img/w/${iconCode}.png`;
+})
+const temperatureCurrent = computed(() => {
+  const { temp } = state.current?.main || {};
+  return Math.round(temp);
+})
+const temperatureMax = computed(() => {
+  return Math.round(state.current?.main.temp_max);
+})
+const temperatureMin = computed(() => {
+  return Math.round(state.current?.main.temp_min);
+})
+const temperatureUnits = computed(() => {
+  return useMetricUnits.value ? 'metric' : 'imperial';
+})
+const temperatureUnitTitle = computed(() => {
+  return useMetricUnits.value ? 'celsius' : 'fahrenheit';
+})
+const temperatureUnitSymbol = computed(() => {
+  return useMetricUnits.value ? '°C' : '°F';
 })
 const UVIndex = computed(() => {
   if (!state.uv) return null;
   return state.uv.value;
 })
+const UVColorClass = computed(() => ({
+  'bg-green-400 text-white': UVIndex.value < 4,
+  'bg-yellow-400': UVIndex.value >= 4 && UVIndex.value <= 8,
+  'bg-red-400 text-white': UVIndex.value > 8,
+}))
 
 // Methods
+const toggleTemptrueUnits = () => {
+  useMetricUnits.value = !useMetricUnits.value;
+}
+// API Calls
 const fetchCurrentData = async (query) => {
-  const queryStr = `${baseApiUrl}/data/2.5/weather?q=${query}&appid=${apiKey}`;
+  const queryStr = `${baseApiUrl}/data/2.5/weather?q=${query}&units=${temperatureUnits.value}&appid=${apiKey}`;
 
   try {
     const response = await axios.get(queryStr);
-    state.current = response.data;
+    return response.data;
   } catch (error) {
-    state.error = error.message;
+    return error.message;
   }
 };
 const fetchForecastData = async (query) => {
-  const queryStr = `${baseApiUrl}/data/2.5/forecast?q=${query}&appid=${apiKey}`;
+  const queryStr = `${baseApiUrl}/data/2.5/forecast?q=${query}&units=${temperatureUnits.value}&appid=${apiKey}`;
 
   try {
     const response = await axios.get(queryStr);
-    state.forecast = response.data;
+    return response.data;
   } catch (error) {
-    state.error = error.message;
+    return error.message;
   }
 };
 const fetchUvData = async () => {
@@ -55,26 +97,47 @@ const fetchUvData = async () => {
 
     try {
       const response = await axios.get(queryStr);
-      state.uv = response.data;
+      return response.data;
     } catch (error) {
-      state.error = error.message;
+      return error.message;
     }
   }
 };
 
 // Lifecycle
-onBeforeMount(() => {
-  fetchCurrentData(city.value);
-  fetchForecastData(city.value);
+onBeforeMount(async () => {
+  state.current = await fetchCurrentData(city.value);
+  state.forecast = await fetchForecastData(city.value);
 })
 
 // Watchers
-watch(coordinates, fetchUvData)
+watch(coordinates, async () => {
+  state.uv = await fetchUvData();
+});
 </script>
 
 <template>
-  <div v-if="state.forecast">
+  <div>
     <h1>Weather Dashboard</h1>
-    <h2>{{ cityName }}</h2>
+    <div v-if="state.current" class="current-panel">
+      <h2>{{ cityName }}</h2>
+      <div class="current">
+        <h3>{{ temperatureCurrent }} {{ temperatureUnitSymbol }}</h3>
+        <div class="img-wrap">
+          <img :src="currentIconUrl" :alt="`${currentWeather} icon`">
+        </div>
+        <p>{{ currentDescription }}</p>
+        <ul>
+          <li v-if="state.uv" :class="['w-max', UVColorClass]">UV Index: {{ state.uv.value }}</li>
+          <li>Max {{ temperatureMax }}{{ temperatureUnitSymbol }}</li>
+          <li>Min {{ temperatureMin }}{{ temperatureUnitSymbol }}</li>
+        </ul>
+      </div>
+
+    </div>
+    <div class="forecast"></div>
+
+    <button @click="toggleTemptrueUnits" class="border p-3 my-3">Toggle Units</button>
+    <div>{{ temperatureUnitTitle }}</div>
   </div>
 </template>
---
+++ src/components/CurrentPanel.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="current-panel">
+        <h2><slot name="city"></slot></h2>
+        <div class="current">
+            <h3><slot name="current-temp"></slot></h3>
+            <div class="img-wrap"><slot name="current-icon"></slot></div>
+            <ul>
+                <slot name="current-desc"></slot>
+            </ul>
+        </div>
+
+    </div>
+</template>
