0adcb0be3bc24313a76fdb617b5d199179f4a99f
Ray Luna <leon.luna.ray@gmail.com>
2023-05-04 22:19:35 -0700

    add sidebar nav

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="sidebar">
+        <h1 class="title">Weather Dashboard</h1>
+        <div class="search">
+            <input type="text" />
+            <button>Serach</button>
+        </div>
+    </div>
+</template>
